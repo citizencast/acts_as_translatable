c2613d197fc143097d628cabdf38dd97967af120
narbobo <nicolas.arbogast@gmail.com>
2010-12-02 14:29:10 +0100

    cette fois-ci, la bonne

--- lib/acts_as_translatable/base.rb
+++ lib/acts_as_translatable/base.rb
@@ -8,7 +8,7 @@ module ActsAsTranslatable
     def acts_as_translatable
       column_names.each do |col|
         if I18n.default_locale == col.split('_').last.to_sym
-          m_name = col.gsub("_#{I18n.locale}", '').to_sym 
+          m_name = col.gsub("_#{col.split('_').last.to_sym}", '').to_sym 
           define_method m_name do
             c_name = self.class.is_model_column?("#{m_name}_#{I18n.locale}") ? "#{m_name}_#{I18n.locale}" : "#{m_name}_#{I18n.default_locale}"
             send(c_name)
